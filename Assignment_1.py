66e19056aa6d3ec9acca976e9e1c2c490a6c6e60
Mridul Thakur <81625773+Mridul-cyber@users.noreply.github.com>
2022-09-10 11:31:07 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,55 @@
+#Assignment 1
+
+#1. Write a command to get the Python version you are using.
+
+"""
+import sys
+print("this is the python version : " + sys.version)"""
+
+
+#2. Write a python script to print Hello Python on the screen directly in the command line.
+
+#print("Hello Python")
+
+
+#3. Write a python script and store it in a file and execute the file from the command line.
+
+#4.  Write a python script to print Hello Python on the screen using visual studio code.
+
+print("Hello Python")
+
+#5.  . Write a python script to print Hello on the first line and Python on the second line
+
+print("Hello","Python" , sep = "\n")
+
+
+#6.   Write a python script to print “MySirG” on the screen
+
+print("\"MySirG\"")
+
+
+#7.  Write a python script to print “Teacher’s Day” on the screen
+
+print("\"Teacher's Day\"")
+
+
+#8. Write a python script to print the value of a variable. Variable contains your name as data
+
+name  = "Mridul"
+print("My name is:" , name)
+
+#9.  Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+name = "Mridul"
+age = 23
+qualification = "B.Tech"
+Years_of_Experience = 1
+
+print("My Name :  "  + name ,"My age:  " +  str(age) ,"My Qualification:  " + qualification ,"My Years of Experience:  " + str(Years_of_Experience) , sep="\n")
+
+
+#10.  Write a python script to print values of three variables, each in a new line. Variable contains integer values
+
+a = 20
+b = 40
+c = 60
+print(a,b,c,sep="\n")
---
+++ Assignment_3.py
@@ -0,0 +1,55 @@
+#1. Write a python script to convert a number into str type
+
+
+
+
+a = 5
+print(str(a))
+
+#2.  Write a python script to print Unicode of the character ‘m'
+
+unicode_m = 'm'
+print(ord(unicode_m))
+
+#3. Write a python script to print character representation of a given unicode 100
+
+char_100 = 100
+print(chr(char_100))
+
+
+#4. Write a python script to print any number and its binary equivalent
+
+a = 15
+print(bin(a))
+
+#5.  Write a python script to print any number and its octal equivalent
+
+print(oct(a))
+
+#6.  Write a python script to print any number and its hexadecimal equivalent.
+
+print(hex(a))
+
+#7. Write a python script to store binary number 1100101 in a variable and print it in decimal format
+
+b = 0b1100101
+
+print(b)
+
+#8. Write a python script to store a hexadecimal number 2F in a variable and print it in octal format.
+
+hexa_D = 0x2F
+
+print(oct(hexa_D))
+
+#9.  Write a python script to store an octal number 125 in a variable and print it in binary format.
+
+oct_no = 0o125
+
+print(bin(oct_no))
+
+
+#10. Write a python script to add two numbers 25 (in octal) and 39 (in hexadecimal) and display the result in binary format.\
+
+
+print(bin(0o25) + bin(0x39))
