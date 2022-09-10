db67a2e7e85a3c75496cdee4223c6aca95794b61
Mridul Thakur <81625773+Mridul-cyber@users.noreply.github.com>
2022-09-10 11:32:57 +0530

    Add files via upload

---
+++ Assignment_4.py
@@ -0,0 +1,100 @@
+#1. Write a python script to take your name as input from the user and then print it.
+
+""" name = input("Enter Your Name")
+print(name)
+ """
+
+
+#2. Write a python script to take input from the user. Input must be a number.
+
+""" number = int(input("Enter a Number"))
+print(number) """
+
+""" 3. Write a python script which takes two numbers from the user, then calculate their sum
+and display the result. """
+
+""" number_1 = int(input("Enter a 1st Number"))
+number_2 = int(input("Enter a 2nd Number"))
+
+Sum = number_1 + number_2 
+
+print(Sum) """
+
+
+
+
+"""4.  Write a python script which takes two numbers from the user, then calculate their sum
+and display the result."""
+
+""" from math import pi
+
+rad = int(input("Enter a radius to get the area of circle " ))
+2
+area = pi * rad**2
+
+print(area) """
+
+
+"""5. Write a python script to calculate the square of a number. Number is entered by the user"""
+
+""" from math import pow
+
+x = int(input("Enter a number: "))
+y = int(input("Square number: "))
+
+tot = pow(x,y)
+
+print(tot) """
+
+"""6. Write a python script to calculate the area of Triangle. Number is entered by the user """
+
+""" b = int(input("Enter a Base"))
+h = int(input("Enter a Height"))
+
+area_Tri = h*b/2
+
+print(area_Tri) """
+
+
+"""7. Write a python script to calculate average of three numbers, entered by the user"""
+
+""" a = int(input("Enter a 1st Number"))
+b = int(input("Enter a 2nd Number"))
+c = int(input("Enter a 3rd Number"))
+
+avg = (a+b+c)/3
+print(avg) """
+
+
+"""8. Write a python script to calculate simple interest
+"""
+""" 
+p = int(input("Principal amo: "))
+r = int(input("Rate of Interest: "))
+t = int(input("Time : "))
+
+simple_Int = p*r*t/100
+
+print(simple_Int) """
+
+
+
+"""9. Write a python script to calculate the volume of a cuboid."""
+""" 
+l = int(input("Enter Lenght: "))
+w = int(input("Enter Width: "))
+h = int(input("Enter Height: "))
+
+vol = l*w*h
+
+print(vol) """
+
+
+"""10. Write a python script to calculate area of a rectangle"""
+
+l = int(input("Enter Length: "))
+w = int(input("Enter Width: "))
+
+area_rec = l*w
+
+print(area_rec)
