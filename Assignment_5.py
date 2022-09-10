6056a30b274012b96970810fe3d7901ba9e33634
Mridul Thakur <81625773+Mridul-cyber@users.noreply.github.com>
2022-09-10 11:35:13 +0530

    Add files via upload

---
+++ Assignment_5.py
@@ -0,0 +1,102 @@
+"""1. Write a python script to remove the last digit from a given number. (for example, if
+user enters 2534 then your output should be 253)"""
+
+""" x  = int(input("enter a number: "))
+
+res  = x//10
+
+print(res) """
+
+
+"""2. Write a python script to get the last digit from a given number. (for example, if user
+enters 2089 then your output should be 9 """
+
+""" x  = int(input("enter a no.  : "))
+
+res = x % 10
+
+print(res) """
+
+
+"""3. Write a python script to swap data of two variables """
+
+""" a  = 10
+b  = 20
+temp = a 
+a  = b  
+b = temp
+
+print("A: %d ""B: %d "%(a,b)) """
+
+
+"""4. Write a python script to find x power y, where values of x and y are given by user
+"""
+
+""" x  = int(input("enter a number: "))
+y = int(input("enter a power: "))
+
+res = x**y
+
+print("X: %d and Y:  %d"%(x,y),"The answer:" ,res) """
+
+
+
+"""5. Write a python script which takes a three digit number from the user and displays
+only its first digit.
+"""
+
+""" 
+x  = int(input("Enter 3 digit number: "))
+
+res = (x//10)//10
+
+print(res) """
+
+
+"""6. Write a python script which takes a three digit number from the user and displays
+only its middle digit.
+"""
+
+""" x  = int(input("Enter 3 digit number: "))
+
+res = (x//10)%10
+
+print(res) """
+
+
+"""7. Write a python script which takes a three digit number from the user and displays
+only its last digit """
+""" 
+x  = int(input("Enter 3 digit number: "))
+
+res = x%10
+
+print(res)
+ """
+
+
+"""8. 8. Write a python script to use IN operator to display the data present in the list
+"""
+
+""" x = "Java","Python","CPP"
+
+print("Java" in x) """
+
+
+"""9. Write a python script to use NOT IN operator to display the data not present in list
+"""
+
+""" x = "Java","Python","CPP"
+
+print("JavaScript" not in x) """
+
+
+"""10. Write a python script to use IS operator to display if both variables are the same
+object or not?"""
+
+""" a  = 50         #a and b are refer. same object 50 and also have same id 
+b = 50
+
+
+print(a is b)       #True
+print(a is not b)   #False """
